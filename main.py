c7c56b311498cc26e7e44edba6c42235f8ee3669
clementmuttoni <clement.muttoni@insa-lyon.fr>
2013-12-21 11:20:32 +0100

    changement 21/12

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# -*-coding: utf-8 -*-
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.properties import NumericProperty, ReferenceListProperty, ObjectProperty
@@ -8,14 +9,31 @@ class JeuPerles(Widget):
 	perleVoyA = ObjectProperty(None)
 	perleVoyB = ObjectProperty(None)
 	
-	def update(self,dt):
-		print("rien pour le moment")
+	
+	
+		
+	
+	
+	
+	#def on_touch_move(self, touch):
+	#		self.perleVoyA.center = touch.pos
+		
    
 
 class Perle(Widget):
+	
+	def __init__(self, voy):
+		voyelle = voy
 		
 	def on_touch_move(self, touch):
+		#if touch.y > self.height/2:
 			self.center = touch.pos
+		#if touch.y < self.height/2:
+		#	self.perleVoyB.center = touch.pos	
+	
+	
+	
+
     
    
 
